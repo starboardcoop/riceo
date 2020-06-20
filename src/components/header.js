722d454bf3076dceeefba9077ef47b29ed1b0170
Dillon Fagan <dillonfagan@pm.me>
2020-06-20 11:58:21 -0400

    Make header scrollable with contact section

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,14 @@ import styles from "./header.module.css"
 const Header = ({ siteTitle }) => (
   <header className={styles.siteHeader}>
     <nav>
+      <Link to="/">About</Link>
       <Link to="/">Learn</Link>
       <Link to="/">Resources</Link>
       <Link to="/">News</Link>
     </nav>
+    <aside>
+      <Link className={styles.button} to="/">Contact</Link>
+    </aside>
   </header>
 )
 
