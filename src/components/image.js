a5ce319cf938366bcd7679ff57a516aeca07de7b
mdantuono <mikeishere3@gmail.com>
2020-06-22 21:27:59 -0400

    added hero image

--- src/components/image.js
+++ src/components/image.js
@@ -16,9 +16,9 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "providence-rhode-island.jpg" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 3000) {
             ...GatsbyImageSharpFluid
           }
         }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,20 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Image />
     <section>
-      <h1>What is a worker cooperative?</h1>
-      <p>There are many kinds of cooperatives, such as credit unions and co-op markets. But unlike other “user-owned” or “consumer-owned” cooperatives, a worker co-op is a business owned by the people doing the work.</p>
-      <p>A worker cooperative (or worker co-op) is a type of business that puts worker wellbeing and community benefit at the core of its purpose.</p>
+      <div className="centered">
+        <h1>What is a worker cooperative?</h1>
+        <p>There are many kinds of cooperatives, such as credit unions and co-op markets. But unlike other “user-owned” or “consumer-owned” cooperatives, a worker co-op is a business owned by the people doing the work.</p>
+        <p>A worker cooperative (or worker co-op) is a type of business that puts worker wellbeing and community benefit at the core of its purpose.</p>
+      </div>
     </section>
     <section>
-      <h1>Who are we?</h1>
-      <p>The Rhode Island Center for Employee Ownership (RI CEO) is a non-profit organization working to promote and support the growing cooperative economy in the Ocean State.</p>
-      <Link className="button" to="/">Learn more</Link>
+      <div className="centered">
+        <h1>Who are we?</h1>
+        <p>The Rhode Island Center for Employee Ownership (RI CEO) is a non-profit organization working to promote and support the growing cooperative economy in the Ocean State.</p>
+        <Link className="button" to="/">Learn more</Link>
+      </div>
     </section>
   </Layout>
 )
