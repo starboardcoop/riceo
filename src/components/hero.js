8013b4d444ca2a35c71211ca770a398cdad0317c
mdantuono <mikeishere3@gmail.com>
2020-06-25 21:25:09 -0400

    added image slider to hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,12 +4,22 @@ import styles from "./hero.module.css"
 
 const Hero = () => (
     <div className={styles.container}>
-        <Image />
-        <section className={styles.heroOverlay}>
-            <h1>Center for Employee Ownership</h1>
-            <h3>Supporting and promoting worker cooperatives in Rhode Island.</h3>
-            <a className="button">Contact</a>
-        </section>
+        <div className={styles.slide}>
+            <Image />
+            <section className={styles.heroOverlay}>
+                <h1>Center for Employee Ownership</h1>
+                <h3>Supporting and promoting worker cooperatives in Rhode Island.</h3>
+                <a className="button">Contact</a>
+            </section>
+        </div>
+        <div className={styles.slide}>
+            <Image />
+            <section className={styles.heroOverlay}>
+                <h1>Center for Employee Ownership</h1>
+                <h3>Supporting and promoting worker cooperatives in Rhode Island.</h3>
+                <a className="button">Contact</a>
+            </section>
+        </div>
     </div> 
 )
 
