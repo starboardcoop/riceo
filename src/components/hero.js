388bceb858e7cd48a8cbc2fc306cb6c84b567f9c
mdantuono <mikeishere3@gmail.com>
2020-06-28 12:46:42 -0400

    image slider sliding

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,24 +3,31 @@ import Image from "../components/image"
 import styles from "./hero.module.css"
 
 const Hero = () => (
-    <div className={styles.container}>
-        <div className={styles.slide}>
-            <Image />
-            <section className={styles.heroOverlay}>
-                <h1>Center for Employee Ownership</h1>
-                <h3>Supporting and promoting worker cooperatives in Rhode Island.</h3>
-                <a className="button">Contact</a>
-            </section>
+    <div >
+        <div className={styles.container}>
+            <div className={styles.slide} id="slide1">
+                <Image />
+                <section className={styles.heroOverlay}>
+                    <h1>Center for Employee Ownership</h1>
+                    <h3>Supporting and promoting worker cooperatives in Rhode Island.</h3>
+                    <a className="button">Contact</a>
+                </section>
+            </div>
+            <div className={styles.slide} id="slide2">
+                <Image />
+                <section className={styles.heroOverlay}>
+                    <h1>Center for Employee Leadership</h1>
+                    <h3>Supporting and promoting worker cooperatives in Rhode Island.</h3>
+                    <a className="button">Contact</a>
+                </section>
+            </div>
+        </div> 
+        <div className={styles.sliderlinks}>
+            <a href="#slide1">&#8226;</a>
+            <a href="#slide2">&#8226;</a>
         </div>
-        <div className={styles.slide}>
-            <Image />
-            <section className={styles.heroOverlay}>
-                <h1>Center for Employee Ownership</h1>
-                <h3>Supporting and promoting worker cooperatives in Rhode Island.</h3>
-                <a className="button">Contact</a>
-            </section>
-        </div>
-    </div> 
+    </div>
+    
 )
 
 export default Hero
