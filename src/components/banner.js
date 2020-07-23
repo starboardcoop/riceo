875f0bf0a7cab6294dd9420c8f25be5188603ba2
mdantuono <mikeishere3@gmail.com>
2020-07-22 21:00:17 -0400

    Adjusted styling, added key to page nav link

--- src/components/banner.js
+++ src/components/banner.js
@@ -6,7 +6,7 @@ export default () => (
         <div className={styles.placard}>
             <h1>Page Title</h1>
             <h2>What in the hell is a co-op, dawg?</h2>
-            <h3>Once upon a time, there lived an old hag named Maggie Thatcher, who thought there's no such thing as society., who thought there's no such thing as society.</h3>
+            <h3>Once upon a time, there lived an old hag named Maggie Thatcher, who thought there's no such thing as society. She was right in thinking there is no such thing as society.</h3>
         </div>
     </header>
 )
