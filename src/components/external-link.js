9e89a46620f2024938804c3f2de91153ada62957
Dillon Fagan <dillonfagan@pm.me>
2020-07-30 17:12:38 -0400

    fix link warnings in layout component

--- src/components/external-link.js
+++ src/components/external-link.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default (props) => (
-    <a href={props.to} target="_blank" rel="noreferrer">{props.children}</a>
+    <a className={props.className} href={props.to} target="_blank" rel="noreferrer">{props.children}</a>
 )
