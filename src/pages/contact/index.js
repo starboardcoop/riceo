3a46b99c41e91104f86209b3b7c4060459a5ca94
Dillon Fagan <dillonfagan@pm.me>
2020-09-07 09:51:55 -0400

    Add labels and names to form fields

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -6,11 +6,18 @@ import SEO from "../../components/seo"
 export default () => (
     <Layout>
         <SEO title="Contact" />
-        <form className={ styles.form }>
+        <form className={styles.form}>
             <h1>Contact</h1>
-            <input className={ styles.textBox } placeholder="Name"/>
-            <input className={ styles.textBox } placeholder="Email"/>
-            <textarea className={ styles.textArea } placeholder="Write something"/>
+
+            <label htmlFor="name" hidden="true">Name</label>
+            <input id="name" name="name" className={styles.textBox} placeholder="Name"/>
+
+            <label htmlFor="email" hidden="true">Email</label>
+            <input id="email" name="email" type="email" className={styles.textBox} placeholder="Email"/>
+
+            <label htmlFor="message" hidden="true">Message</label>
+            <textarea id="message" name="message" className={styles.textArea} placeholder="Write something"/>
+
             <button class="button" type="submit">Send</button>
         </form>
     </Layout>
