bf017ec2cbb9ccb8c85cbab96323637afa2209bf
Dillon Fagan <dillonfagan@pm.me>
2021-04-24 10:30:07 -0400

    Style banner and align with section

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 export default (props) => (
-    <header>
-        <div>
+    <header className="bg-gray-300">
+        <div className="bg-white bg-opacity-50 mx-auto px-4 py-8 md:w-2/3 lg:w-1/2">
             <h1>{props.title}</h1>
             <h2>{props.subtitle}</h2>
             <h3>{props.content}</h3>
--- src/components/section.js
+++ src/components/section.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 const Section = (props) => (
-    <section id={props.id} className="p-4">
-        <div className="mx-auto md:w-2/3 lg:w-1/2">
+    <section id={props.id}>
+        <div className="mx-auto p-4 md:w-2/3 lg:w-1/2">
             {
                 props.html != null &&
                 <div dangerouslySetInnerHTML={{__html: props.html}}></div>
