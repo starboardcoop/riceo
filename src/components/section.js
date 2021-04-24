685d78bf6e03460bcb5b195092c92ec6abaafd53
Dillon Fagan <dillonfagan@pm.me>
2021-04-24 10:02:08 -0400

    Limit width of section content

--- src/components/section.js
+++ src/components/section.js
@@ -1,17 +1,19 @@
 import React from "react"
 
 const Section = (props) => (
-    <section id={props.id}>
-        {
-            props.html != null &&
-            <div dangerouslySetInnerHTML={{__html: props.html}}></div>
-        }
-        {
-            props.html == null &&
-            <div>
-                {props.children}
-            </div>
-        }
+    <section id={props.id} className="p-4">
+        <div className="mx-auto md:w-2/3 lg:w-1/2">
+            {
+                props.html != null &&
+                <div dangerouslySetInnerHTML={{__html: props.html}}></div>
+            }
+            {
+                props.html == null &&
+                <div>
+                    {props.children}
+                </div>
+            }
+        </div>
     </section>
 )
 
