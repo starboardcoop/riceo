79fa073b9160e8e48a6cd21d74ca061355598a53
Dillon Fagan <dillonfagan@pm.me>
2021-04-24 15:28:24 -0400

    Improve nav structure

--- src/components/header.js
+++ src/components/header.js
@@ -20,11 +20,11 @@ const Links = () => (
 
     render={data => (
       <>
-        <Link className="text-lg font-bold" to="/">
+        <Link to="/">
           <img src={logo} alt={data.navigationJson.logo.alt} className="max-h-11" />
         </Link>
         {data.navigationJson.links.map(link => (
-          <Link className="w-max text-lg rounded-full px-4 py-1 transition  hover:bg-secondary hover:bg-opacity-40" activeClassName="bg-secondary" to={link.link} key={link.label}>{link.label}</Link>
+          <Link className="w-max text-lg rounded-full px-4 py-1 transition hover:bg-secondary hover:bg-opacity-40" activeClassName="bg-secondary" to={link.link} key={link.label}>{link.label}</Link>
         ))}
       </>
     )}
@@ -32,13 +32,13 @@ const Links = () => (
 )
 
 const Header = () => (
-  <header className="bg-white flex flex-row items-center overflow-x-auto mx-auto py-4 shadow-sm">
-    <nav className="flex flex-row flex-grow items-center space-x-6 pl-8">
+  <header className="bg-white flex flex-row items-center space-x-6 overflow-x-auto mx-auto px-4 md:px-8 py-4 shadow-sm">
+    <nav className="flex flex-row items-center space-x-6">
       <Links />
     </nav>
-    <aside className="flex flex-row-reverse pr-8">
+    <div className="flex flex-row flex-grow justify-end items-center space-x-6">
       <Link className="button" to="/contact">Contact</Link>
-    </aside>
+    </div>
   </header>
 )
 
--- src/components/page-navigation.js
+++ src/components/page-navigation.js
@@ -1,9 +1,9 @@
 import React from "react"
 
 export default (props) => (
-    <div className="w-full bg-secondary px-8 py-2">
-        <nav className="flex flex-row items-center space-x-4">
-            { props.sections.map(section => <a href={`#${section.id}`} key={section.id} className="rounded-full px-3 py-1 text-sm hover:bg-primary hover:text-white">{section.title}</a>) }
+    <div className="w-full bg-secondary flex flex-row px-4 md:px-8 py-2 overflow-x-auto">
+        <nav className="flex flex-row items-center space-x-3">
+            { props.sections.map(section => <a href={`#${section.id}`} key={section.id} className="w-max rounded-full px-3 py-1 text-sm hover:bg-primary hover:text-white">{section.title}</a>) }
         </nav>
     </div>
 )
