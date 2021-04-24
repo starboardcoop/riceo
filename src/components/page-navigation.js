0d1fdeba8f50356ce41c93eeaed08d5df26e657d
Dillon Fagan <dillonfagan@pm.me>
2021-04-24 10:38:07 -0400

    Adjust page navigation link padding

--- src/components/page-navigation.js
+++ src/components/page-navigation.js
@@ -3,7 +3,7 @@ import React from "react"
 export default (props) => (
     <div className="w-full bg-secondary px-8 py-2">
         <nav className="flex flex-row items-center space-x-4">
-            { props.sections.map(section => <a href={`#${section.id}`} key={section.id} className="rounded-full px-4 py-1 text-sm hover:bg-primary hover:text-white">{section.title}</a>) }
+            { props.sections.map(section => <a href={`#${section.id}`} key={section.id} className="rounded-full px-3 py-1 text-sm hover:bg-primary hover:text-white">{section.title}</a>) }
         </nav>
     </div>
 )
