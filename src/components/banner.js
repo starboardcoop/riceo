72c6ce6a9fd8c771b894cf07844fb1a428bf774e
Dillon Fagan <dillonfagan@pm.me>
2021-04-25 01:01:02 -0400

    Adjust banner heading size

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,8 +4,8 @@ export default (props) => (
     <header className="bg-gray-300 mb-6">
         <div className="bg-white bg-opacity-50 mx-auto px-4 py-8 md:w-2/3 lg:w-1/2">
             <h1>{props.title}</h1>
-            <h2>{props.subtitle}</h2>
-            <h3>{props.content}</h3>
+            <h2 className="text-xl">{props.subtitle}</h2>
+            <h3 className="text-lg">{props.content}</h3>
         </div>
     </header>
 )
